ff68171f351802f71ab53e4abc5db308d582df37
ice-c0der <75302897+ice-c0der@users.noreply.github.com>
2020-12-01 13:19:26 +0300

    Update main.py
    last commit

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ class Prog(QMainWindow):
         self.repaint()
 
     def draw(self, qp):
-        r, b, g = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
-        qp.setBrush(QColor(a, b, c))
+        qp.setBrush(QColor(255, 255, 0))
         a = random.randint(100, 300)
         qp.drawEllipse(10, 10, a, a)
 
